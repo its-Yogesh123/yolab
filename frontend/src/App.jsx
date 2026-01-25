52198000da4fc991de229e4ab8f4896ec5ef8b2e
Yogesh Kumar <yogesh.developer9@gmail.com>
2026-01-25 19:13:24 +0530

    Set-up Deplyment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const App = ()=>{
+    return (
+        <div>
+            <h1>Under Construction</h1>
+            <p>We will back soon.......</p>
+        </div>
+    );
+};
