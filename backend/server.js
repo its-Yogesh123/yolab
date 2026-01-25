9ea61bb58d8aa213b57cc409f114c2a1a3c78490
Yogesh Kumar <yogesh.developer9@gmail.com>
2026-01-25 19:15:17 +0530

    Bug Fix

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    return res.end("<html><h1> We will back soon !!!! </h1></html>");
+});
+app.listen(8000,()=>{console.log("Server Started")});
