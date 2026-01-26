427fd06371c1d6c6dd2d7e8278903c97876e7801
Yogesh Kumar <yogesh.developer9@gmail.com>
2026-01-26 20:17:52 +0530

    Frontend Fix bug

--- frontend/src/components/ui/homePage.jsx
+++ frontend/src/components/ui/homePage.jsx
@@ -5,7 +5,7 @@ import { SparklesCore } from "./sparkles";
 export function LandingPage() {
   return (
     <div
-      className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      className="h-[100vh] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <h1
         className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
         YoLab is building....
